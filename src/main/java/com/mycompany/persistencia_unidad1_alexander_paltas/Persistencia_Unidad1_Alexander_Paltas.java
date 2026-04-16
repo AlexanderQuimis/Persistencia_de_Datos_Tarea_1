d135c269cfa4aef7fff09ccac24b995bae38ff56
alexa <alexa@Alecs>
2026-04-16 13:10:27 -0500

    Conexión de Netbenas a Github

---
+++ src/main/java/com/mycompany/persistencia_unidad1_alexander_paltas/Persistencia_Unidad1_Alexander_Paltas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persistencia_unidad1_alexander_paltas;
+
+/**
+ *
+ * @author alexa
+ */
+public class Persistencia_Unidad1_Alexander_Paltas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
